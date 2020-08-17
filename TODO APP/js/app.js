eb94e55d81a2cf236cc2ee1280db293a8d885dc3
amina-sabir-ali <aminasabir6700@gmail.com>
2020-08-17 22:38:59 +0500

    TODO APP

--- TODO APP/js/app.js
+++ TODO APP/js/app.js
@@ -1,51 +1,97 @@
-var list_item = document.getElementById('list-item')
+var list_item = document.getElementById('list-item');
+
 function addTodo(){
-   var todo_item = document.getElementById('todo-item');
-   // Create li tag with text node
-   var li = document.createElement('li');
-   var Text = document.createTextNode(todo_item.value);
-   li.setAttribute("class","todo_item");
-   li.appendChild(Text);
+    var todo_item = document.getElementById('todo_input');
+    // Create li tag with text node
+    var li = document.createElement('li');
+    // var Text = document.createTextNode(todo_item.value);
+    // li.appendChild(Text);
 
-   // Edit Button
-   var editBtn = document.createElement('button');
-   var editText = document.createTextNode("Edit");
-   editBtn.appendChild(editText);
-   
-   editBtn.setAttribute("class","editbtn");
-   editBtn.setAttribute("class","btn btn-dark ml-4");
-   editBtn.setAttribute("onclick","editItem(this);");
-   li.appendChild(editBtn);
-   
-   // Delete Button
-   var delBtn = document.createElement('button');
-   var delText = document.createTextNode('Delete');
-   delBtn.appendChild(delText);
+    // create div element group
+    var divElement = document.createElement('div');
+    divElement.setAttribute('class','input-group mt-2');
+    li.appendChild(divElement);
 
-   delBtn.setAttribute("class","delbtn");
-   delBtn.setAttribute("class","btn btn-dark ml-1");
-   delBtn.setAttribute("onclick","deleteItem(this)")
-   li.appendChild(delBtn);
-  
+    //create input in div class input-group
+    var output = document.createElement('input');
+    output.setAttribute("type","text");
+    output.setAttribute("class","form-control");
+    output.setAttribute("aria-describedby","button-addon4");
+    output.value = todo_item.value;
+    output.setAttribute("readonly","true");
+    divElement.appendChild(output);
+     
+    //create div element with class input-group-append
+    var btnGroupDiv = document.createElement('div');
+    btnGroupDiv.setAttribute('class','input-group-append');
+    btnGroupDiv.setAttribute('id','button-addon4');
+    divElement.appendChild(btnGroupDiv); 
+    
+    // create check button
+    var checkBtn = document.createElement('button');
+    checkBtn.setAttribute("class","btn btn-dark");
+    var checkText = document.createElement('i');
+    checkText.setAttribute("class","fas fa-check");
+    checkBtn.appendChild(checkText);
+    btnGroupDiv.appendChild(checkBtn);
 
-   list_item.appendChild(li);
-   console.log(list_item);
-   todo_item.value = "";
+    // create edit button
+    var editBtn = document.createElement('button');
+    editBtn.setAttribute("class","btn btn-dark");
+    var editText = document.createElement('i');
+    editText.setAttribute("class","fas fa-pen");
+    editBtn.appendChild(editText);
+    btnGroupDiv.appendChild(editBtn);
+
+    // create delete button
+    var delBtn = document.createElement('button');
+    delBtn.setAttribute("class","btn btn-dark");
+    var delText = document.createElement('i');
+    delText.setAttribute("class","fas fa-trash");
+    delBtn.appendChild(delText);
+    btnGroupDiv.appendChild(delBtn);
+
+    li.appendChild(divElement)
+    console.log(li);
+    // // create delete button
+    // var delBtn = document.createElement('button');
+    // delBtn.setAttribute("class","btn btn-dark mt-2 mr-1");
+    // var delText = document.createElement('i');
+    // delText.setAttribute("class","fas fa-trash");
+    // delBtn.appendChild(delText);
+
+    checkBtn.setAttribute("onclick","deleteItem(this)")
+    delBtn.setAttribute("onclick","deleteItem(this)")
+    // li.appendChild(delBtn);
+
+    // // create delete button
+    // var editBtn = document.createElement('button');
+    // editBtn.setAttribute("class","btn btn-dark mt-2");
+    // var editText = document.createElement('i');
+    // editText.setAttribute("class","fas fa-pen");
+    
+    editBtn.setAttribute("onclick","editItem(this)");    
+    // editBtn.appendChild(editText);
+
+    // li.appendChild(editBtn);
+
+    list_item.appendChild(li);
+    todo_item.value = "";
 }
 
-
 function deleteItem(e){
-   e.parentNode.remove();
+//    e.parentNode.remove();
+      e.parentNode.parentNode.parentNode.remove();
 }
 
 function editItem(e){
-   var editValue = prompt("Enter edit value",e.parentNode.firstChild.nodeValue);
-   // console.log(e.parentNode.childNodes[0]);
-   // console.log(e.parentNode.firstChild);
-   console.log(editValue);
-   e.parentNode.firstChild.nodeValue = editValue;
+    // var readonlyValue = e.parentNode.parentNode.firstChild.getAttribute('readonly');
+    var newValue = prompt("Edit the todo list",e.parentNode.parentNode.firstChild.value);
+    e.parentNode.parentNode.firstChild.value = newValue;
+    // var newValue = prompt("Edit the todo list",e.parentNode.firstChild.nodeValue);
+    // e.parentNode.firstChild.nodeValue = newValue;
 }
 
 function deleteAll(){
-   list_item.innerHTML = "";
+    list_item.innerHTML = "";
 }
